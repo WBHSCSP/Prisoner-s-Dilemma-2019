e34dc8f454987b54eec7c5041e3cb74b7d6ae130
McBasco1 <KynanFletcher28@gmail.com>
2020-01-22 09:42:51 -0600

    Return C at the end so it won't break

--- team1.py
+++ team1.py
@@ -84,8 +84,8 @@ def move(my_history, their_history, my_score, their_score):
                     return 'b'
                 else:
                     return 'c'
+    return 'c'
 
-    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
